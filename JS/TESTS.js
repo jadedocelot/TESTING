7c22d506c24287f63a47bd25c28bd6fe7156c9d4
0rt <jadedocelot@gmail.com>
2020-04-29 00:10:10 -0700

    - Learning C++

--- JS/TESTS.js
+++ JS/TESTS.js
@@ -1,11 +1,11 @@
-function guessmyAge(myVal) {
-    if (myVal == 28) // if myVal is 28 then == will retrieve TRUE  
-    // Eaqualiy Op (==) compares two values and returns TRUE if equivilant or FALSE if not
-        return "This is my correct age!"; // TRUE 
-{
+function strictOp(val) {
+    if  (val === 7)
+        return 'This is true!';
+        {
 
-        return "This is not my correct age"; //FALSE
-}
+        return 'This is false.';
+        
+        }
 
 }
 
@@ -13,3 +13,6 @@ console.log(guessmyAge('28'));  ///consider this a TEST INPUT for now ANCHOR
 //"This is my correct age!" will print out in DEV CON since 28 is TRUE
 console.log(guessmyAge(67));
 //"This is not my correct age" will print out in DEV CON since 67 is FALSE
+
+console.log(strictOp('7'));
+console.log()
