64c6bab66040ade414d09fd119ce0f2c8b64c314
0rt <jadedocelot@gmail.com>
2020-05-10 23:10:52 -0700

    Last night in Oregon

--- JS/TESTS.js
+++ JS/TESTS.js
@@ -1,19 +1,43 @@
-function dontDiscriminate(val) {
+/* function phoneticLookup(val) {
+	var result = "";
+  
+	  var lookup = {
+	  	"alpha":"Adams",
+	  	"bravo":"Boston",
+	  	"charlie":"Chicago",
+	  	"delta":"Denver",
+	  	"echo": "Easy",
+	  	"foxtrot": "Frank",
+	};
+	  var value = val;
+	  result = lookup[value];
+  
+	return result;
+  }
 
-EqualTo = "";
-
-switch(val) {
-case 1:
-case 2:
-case 3:
-	EqualTo ="This worked";
+  console.log(phoneticLookup("delta")) // Denver */
 
 
-}
+  function whatsForDinner(val){
+	result = ""; //result will return  a string
+	
+	var dinnerOptions = { /// Our object using in place of a SWITCH/CASE 
+		//Keyvalue pairs listed below 
+		1:"Greek", //value 1 
+		2:"Itallian", //value 2
+		3:"Indian", // value 3
+		4:"Mexican" //value 4  
 
-return EqualTo
 
-}
+ 
+		
+	};
 
-console.log(dontDiscriminate(2))
-console.log(dontDiscriminate(6))
+	var value = val; // var for (whatForDinner) function
+		result = dinnerOptions[value];
+
+
+		return result;
+	}
+
+		console.log(whatsForDinner(1))
