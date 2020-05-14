26ee7831616f19a03e0f77b2705353972971db7c
0rt <jadedocelot@gmail.com>
2020-05-14 16:19:32 -0700

    GG

--- JS/TESTS.js
+++ JS/TESTS.js
@@ -1,2 +1,36 @@
 //A JS object one way to handle flexable data  
 //They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects
+
+var californiaHouseHold = [
+	{
+		"Adult": "Edgar",
+		"height": "FiveFeet",
+		"HairColor": "brown", 
+		"Clothes": [ 
+			"shirt",
+			"shorts",
+			"headphones",
+			"beanie",
+		],
+
+		"Alive": true
+	}, 
+
+	{
+		"dog": "otto",
+		"breed": "snauzer",
+		"color": "black",
+		"fave_snacks": [
+			"chicken",
+			"pizza",
+			"mondays"
+		],
+
+			"Alive": true
+	
+	}
+]; 
+
+
+
+console.log("Pet 1:" + JSON.stringify(californiaHouseHold[1]))/
--- JS/index.js
+++ JS/index.js
@@ -1442,9 +1442,38 @@ var myObj = {
 	
 	// Test your code by modifying these values
 
+ /* END */ 
 
+ <-- Manipulating complex objects -->
 
+ //A JS object one way to handle flexable data  
+//They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects
+var californiaHouseHold = [
+	{
+		"Adult": "Edgar",
+		"height": "FiveFeet",
+		"HairColor": "brown", 
+		"Clothes": [ 
+			"shirt",
+			"shorts",
+			"headphones",
+			"beanie",
+		],
+		"Alive": true
+	}, 
+	{
+		"dog": "otto",  //Property
+		"breed": "snauzer",
+		"color": "black",
+		"fave_snacks": [ // Array
+			"chicken",
+			"pizza",
+			"mondays"
+		],
 
+            "Alive": true
+            
+	}
+]; 
 
-
-<-- -->
+console.log("Pet 1:" + JSON.stringify(californiaHouseHold[1]))/
