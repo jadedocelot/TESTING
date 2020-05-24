a88d03fa06a678272544756a5be39084a1845995
0rt <jadedocelot@gmail.com>
2020-05-23 21:18:00 -0700

    index.js

--- JS/index.js
+++ JS/index.js
@@ -1712,8 +1712,8 @@ for (var i = 1; i < 6; i++) {
 
  // (var i = 1;) = initialization / I has a value of 1
  // i < 6: = condition / This will count on till it evaluates to 5 (FALSE)
- //I++ = final-expression; increment (i) by 1
-
+ // I++ = final-expression; increment (i) by 1 (i-- decrements)
+ /* exit */
 
 
 
