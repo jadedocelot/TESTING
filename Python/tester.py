ffc32de74b740a94714fc5789fd0d025a83797eb
0rt <jadedocelot@gmail.com>
2020-05-28 00:09:45 -0700

    Another night

--- Python/tester.py
+++ Python/tester.py
@@ -1,10 +1,10 @@
 winner = 28
-guess = int(input("Enter an integer :"))
+guess = int(raw_input('Enter an integer : '))
 
-while guess == winner: 
+if guess == winner: 
     print "Congratulations! You guessed it!."
-    print "(but you don't get any prizes.....HA HA)"
-if guess < winner:
+    print "(but you don't get any prizes.....HA HA"
+elif guess < winner:
     print "Nope! It is a little higher than that."
 else:
     print "No, it is a little lower than that."
